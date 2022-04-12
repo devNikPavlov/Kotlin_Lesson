f39abf28be986f6c655aacb456fa0ef929dedfae
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-04-12 17:19:48 +0300

    03. конструкция if

---
+++ src/lessons/les_03/Main.kt
@@ -0,0 +1,11 @@
+package lessons.les_03
+
+fun main(args: Array<String>) {
+    val x = 3
+    val y = 1
+    if (x > y) {
+        println("x больше y")
+    }
+
+    println("Если вы видите выше строку - то условие верно")
+}
