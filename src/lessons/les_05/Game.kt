c8fd7fd9bafef79644108d0b4e0d2c75a83b84a3
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-06-03 19:07:38 +0300

    05.2 Создание цикла ввода пользователем слов и проверка правельности ввода

--- src/lessons/les_05/Game.kt
+++ src/lessons/les_05/Game.kt
@@ -3,7 +3,34 @@ package lessons.les_05
 fun main() {
     val option = arrayOf("Камень", "Ножницы", "Бумага")
     val gameChoice = getGameChoice(option)
+    // Вызов функции getUserChoice
+    val userChoice = getUserChoice(option)
 }
 
 
 fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random() * optionsParam.size).toInt()]
+
+fun getUserChoice(optionsParam: Array<String>): String {
+    // Эта переменая будет показывать, ввел ли пользователь допустимый вариант
+    var isValidChoice = false
+    var userChoice = ""
+    // Выпольнять цикл, пока пользователь не введет допустимый вариант
+    while (!isValidChoice) {
+
+        // Запросить у пользователя его выбор
+        print("Пожалуйста введите одно слово из предложенных: ")
+        for (item in optionsParam) print(" $item")
+        print(".")
+        //Прочитать пользовательский ввод
+        val userInput = readLine()
+        // Проверить пользовательский ввод
+        if (userInput != null && userInput in optionsParam) {
+            // если ввод пользователя прошел проверку, цикл прерывается
+            isValidChoice = true
+            userChoice = userInput
+        }
+        // если выбран недопустимый вариант, сообщить пользователю
+        if (!isValidChoice) println("Ты ввел неправельное слово")
+    }
+    return userChoice
+}
