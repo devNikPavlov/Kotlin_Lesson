2c5793ee53bcac64deae018f73851b0cc310ae4a
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-06-03 17:01:47 +0300

    04. Функции создание и вызов

---
+++ src/lessons/les_04/App.kt
@@ -0,0 +1,14 @@
+package lessons.les_04
+
+/*
+Создание функций и вызов функций
+ */
+
+fun main(args: Array<String>) {
+    printSum(5,6)
+}
+
+fun printSum(int1: Int, int2: Int) {
+    val result = int1 + int2
+    println(result)
+}
